5c9aaf1875c33d67612df1b0e5573ec03fd1a237
Ben Cave <bencaveuk@gmail.com>
2023-01-23 17:38:19 +0000

    deploy prep1

--- config/environment.js
+++ config/environment.js
@@ -4,8 +4,8 @@ export const PORT = 5001
 export const MONGODB_CONNECTION_STRING = 'mongodb://127.0.0.1:27017/natural-beauty'
 
 //Import data commands to mongodb atlas
-//mongoimport --uri mongodb+srv://benscave:9H1UwHStOkwDuOtd@parks.7png9bz.mongodb.net/parks --collection parks --type json --file parks.json
-//mongoimport --uri mongodb+srv://benscave:9H1UwHStOkwDuOtd@parks.7png9bz.mongodb.net/parks --collection users --type json --file users.json
+//mongoimport --uri mongodb+srv://benscave:<>@parks.7png9bz.mongodb.net/parks --collection parks --type json --file parks.json
+//mongoimport --uri mongodb+srv://benscave:<>@parks.7png9bz.mongodb.net/parks --collection users --type json --file users.json
 
 // SECRET for authentification
 export const SECRET = '^9K2fn;F{cT"#)UL'
